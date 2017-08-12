20104906038b998c5a95da3312395bc12a364d2e
tolgahantunc <tunctolgahan@gmail.com>
2017-08-13 00:11:09 +0300

    n-gram ornegi
    nltk kutuphanesi kullanilarak hazirlanmis n-gram uygulamasi ornegi

---
+++ nGram.py
@@ -0,0 +1,14 @@
+import nltk
+from nltk import ngrams
+from nltk.collocations import *
+
+sentence = 'bu cumle, n-gram denemesi icin yazilmis bir deneme cumlesidir.'
+tokens = nltk.word_tokenize(sentence)
+
+#Create your bigrams
+bgs = nltk.ngrams(tokens,2)
+
+#compute frequency distribution for all the bigrams in the text
+fdist = nltk.FreqDist(bgs)
+for k,v in fdist.items():
+    print (k,v)
