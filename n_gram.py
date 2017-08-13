70163d29c12e87f249d2711dfa557a12d102e81c
Ozgur Firat Cinar <ozgurfiratcinar@gmail.com>
2017-08-13 23:10:21 +0300

    fixed pep-8 rules

--- n_gram.py
+++ n_gram.py
@@ -5,10 +5,10 @@ from nltk.collocations import *
 sentence = 'bu cumle, n-gram denemesi icin yazilmis bir deneme cumlesidir.'
 tokens = nltk.word_tokenize(sentence)
 
-#Create your bigrams
+# Create your bigrams
 bgs = nltk.ngrams(tokens,2)
 
-#compute frequency distribution for all the bigrams in the text
+# compute frequency distribution for all the bigrams in the text
 fdist = nltk.FreqDist(bgs)
 for k,v in fdist.items():
     print (k,v)
